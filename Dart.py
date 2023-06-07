8ab50d4f581b0d6835bd1c1cd2375e47a7e0b0d2
pervin0527 <zfbtldnz@gmail.com>
2023-06-08 00:36:04 +0900

    Updated 2023.06.08

--- Dart.py
+++ Dart.py
@@ -1,89 +1,101 @@
 import io
-import re
 import json
-import requests
 import zipfile
+import requests
 import pandas as pd
 import xml.etree.ElementTree as ET
-from datetime import date
 
-APIS = [
-    {"고유번호" : "https://opendart.fss.or.kr/api/corpCode.xml"},
-    {"상장기업 재무정보" : "https://opendart.fss.or.kr/api/fnlttSinglAcnt.json"},
-    {"단일회사 전체 재무제표" : "https://opendart.fss.or.kr/api/fnlttSinglAcntAll.json"}
-]
 
 class Dart:
-    def __init__(self, crtfc_key, save_path, exclude_pattern=None):
-        # self.key_path = "./API_KEY.txt"
-        # self.crtfc_key = self.read_api_key()
-        self.crtfc_key = crtfc_key
-        self.exclude_pattern = exclude_pattern
-        self.save_path = save_path
+    def __init__(self):
+        self.API_KEY = self.get_key()
+        self.targets = self.basic_processing()
+
+        self.URLS = {"고유번호" : "https://opendart.fss.or.kr/api/corpCode.xml",
+                     "단일회사 전체 재무제표" : "https://opendart.fss.or.kr/api/fnlttSinglAcntAll.json",
+                     "공시서류원본" : "https://opendart.fss.or.kr/api/document.xml"}
         
-    def read_api_key(self):
-        with open(self.key_path, "r") as f:
+        self.name_to_code = {"1분기보고서" : "11013",
+                             "반기보고서" : "11012",
+                             "3분기보고서" : "11014",
+                             "사업보고서" : "11011"}
+        
+        self.search_unique_number()
+        self.get_financial_document()
+
+        
+    def get_key(self):
+        with open("./API_KEY.txt", "r") as f:
             api_key = f.read()
 
         return api_key
-    
-    def open_request(self, res_raw):
-        zfile = zipfile.ZipFile(io.BytesIO(res_raw.content))
-        fin = zfile.open(zfile.namelist()[0]) ## CORPCODE.xml type -> list
-        root = ET.fromstring(fin.read().decode("utf-8"))
 
-        return root
 
-    def search_company(self, root, target_name):
-        for child in root:
-            corp_name = child.find("corp_name").text.strip()
-            
-            if corp_name == target_name:
-                corp_code = child.find("corp_code").text.strip()
-                stock_code = child.find("stock_code").text.strip()
-                modify_date = child.find("modify_date").text.strip()
-                
-                return {"corp_name" : corp_name, 
-                        "corp_code" : corp_code,
-                        "stock_code" : stock_code, 
-                        "latest modified date" : modify_date}
+    def basic_processing(self):
+        df = pd.read_csv("./company_list.csv")
+        rows_all_filled = list(df.notnull().all(axis=1))
+
+        targets = []
+        for idx, row in df.iterrows():
+            if rows_all_filled[idx] != False:
+                targets.append({"company_name" : row[0],
+                                "years" : row[1],
+                                "report_types" : row[2],
+                                "bs_types" :row[3]})
+
+        for target in targets:
+            for idx, (key, value) in enumerate(target.items()):
+                if idx > 0:
+                    value = value.split(',')
+                    target[key] = value
         
-        return "Sorry, Nothing Found."
+        return targets
 
-    def get_corp_info(self, target_name):
-        res = requests.get(APIS[0]["고유번호"], params={"crtfc_key" : self.crtfc_key})
-        root = self.open_request(res)
-        data = self.search_company(root, target_name)
+
+    def send_request(self, request_name, parameters):
+        ## Send_request
+        response = requests.get(self.URLS[request_name], params=parameters)
+
+        ## Open data
+        zfile = zipfile.ZipFile(io.BytesIO(response.content))
+        data = zfile.open(zfile.namelist()[0]) ## CORPCODE.xml type -> list
+        data = ET.fromstring(data.read().decode("utf-8"))
 
         return data
-    
-    def get_financial_data(self, corp_code, target_list, boundary):
-        tmp_date = date.today()
-        tmp_year = tmp_date.year
 
-        result_set = {}
-        for i in range(boundary, 0, -1):
-            params={"crtfc_key" : self.crtfc_key,
-                    "corp_code" : corp_code, 
-                    "bsns_year" : str(tmp_year - i),
-                    "reprt_code" : str(11011),
-                    "fs_div" : "CFS"}
-        
-            res = requests.get(APIS[2]["단일회사 전체 재무제표"], params=params)
-            json_dict = json.loads(res.text)
-            dataset = json_dict["list"]
-            
-            subjects = []
-            subject_values = []
-            for data in dataset:
-                for target in target_list:
-                    account_name = data["account_nm"]
-                    if re.search(target, account_name) and not re.search(self.exclude_pattern, account_name):
-                        subjects.append(account_name)
-                        current_amount = format(int(data["thstrm_amount"][:-6]), ",")
-                        subject_values.append(current_amount)
 
-            result_set.update({str(tmp_year - i) : subject_values})
+    def search_unique_number(self):
+        parameters = {"crtfc_key" : self.API_KEY}
+        response_data = self.send_request("고유번호", parameters)
+
+        for target in self.targets:
+            for data in response_data:
+                corp_name = data.find("corp_name").text.strip()
+                
+                target_name = target["company_name"]
+                if corp_name == target_name:
+                    corp_code = data.find("corp_code").text.strip()
+                    stock_code = data.find("stock_code").text.strip()
+                    modify_date = data.find("modify_date").text.strip()
+                    
+                    target.update({"corp_name" : corp_name, 
+                                   "corp_code" : corp_code,
+                                   "stock_code" : stock_code, 
+                                   "latest modified date" : modify_date})        
+        print(self.targets)
         
-        df = pd.DataFrame(result_set, index=subjects)
-        df.to_csv(self.save_path, encoding="utf-8-sig")
+
+    def get_financial_document(self):
+        for target in self.targets:
+            for year in target["years"]:
+                for report in target["report_types"]:
+                    for bs_type in target["bs_types"]:
+                        parameters = {"crtfc_key" : self.API_KEY,
+                                      "corp_code" : target["corp_code"],
+                                      "bsns_year" : str(year),
+                                      "reprt_code" : self.name_to_code[report],
+                                      "fs_div" : bs_type.upper()}
+                        
+                        ## TODO 이 부분 수정 필요. 접수번호 rcept_no 가져오고, 공시서류원본 가져오기.
+                        response_data = self.send_request("단일회사 전체 재무제표", parameters)
+                        print(response_data)
---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from Dart import Dart
+
+if __name__ == "__main__":
+    dart = Dart()
